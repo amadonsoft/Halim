91f8a3bf57f498c31fa0a30d221173eab9f47786
Oss <dondon98@tpg.com.au>
2019-10-24 22:31:26 +0800

    Initial Commit

---
+++ Halim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Halim
+//
+//  Created by Oss on 24/10/19.
+//  Copyright © 2019 Oss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
